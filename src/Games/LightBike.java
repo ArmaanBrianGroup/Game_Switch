f47a65fcd91bdfff512d57d2b47589059fb30ccb
TheSpicyMango <pwnomancer@gmail.com>
2017-05-17 00:31:06 -0400

    primitive

---
+++ src/Games/LightBike.java
@@ -0,0 +1,22 @@
+package Games;
+
+import java.util.ArrayList;
+
+import Main.Image;
+
+public class LightBike extends Game{
+
+	private int aX, aY, bX, bY;
+	
+	public LightBike(Image image, ArrayList<Integer> keys) {
+		super(image, keys);
+	}
+
+	public void run() {
+		resetDone();
+		while(done == false) {
+			
+		}
+	}
+	
+}
