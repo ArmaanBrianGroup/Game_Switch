c45fc4c1df1d8a947ccb1d85703905b12d5a7d11
armaangoel78 <armaangoel78@gmail.com>
2017-05-17 11:00:10 -0400

    negative rect draws & primitive lightbike

--- src/Games/LightBike.java
+++ src/Games/LightBike.java
@@ -2,6 +2,7 @@ package Games;
 
 import java.awt.Color;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import Main.Image;
 import Main.Input_Handler;
@@ -11,38 +12,67 @@ public class LightBike extends GameScreen {
 	
 	
 
-	private int x1, y1, x2, y2, speed1, speed2;
-	private int v1, v2; //0 up, 1 down, 2 left, 3 right
+	private int x1, y1, x2, y2, speed1, speed2, size1, size2;
+	private int v1, v2, v1Last, v2Last; //0 up, 1 down, 2 left, 3 right
+	private ArrayList<Integer> lineX1, lineX2, lineY1, lineY2;
+	
 	
 	public LightBike(Image image, Input_Handler handler) {
 		super(image, handler);
-		// TODO Auto-generated constructor stub
+
+		x1 = 400; y1 = 400;
+		x2 = 100; y2 = 100; System.out.println("CALLED");
+		v1 = 0; v2 = 1;
+		speed1 = 7; speed2 = 7;
+	
+		size1 = 20; size2 = 20;
+		
+		lineX1 = new ArrayList<Integer>();
+		lineY1 = new ArrayList<Integer>();
+		lineX2 = new ArrayList<Integer>();
+		lineX2 = new ArrayList<Integer>();
 	}
 
 	protected void draw() {
 
-		image.drawRect(Color.red, x1, y1, 20, 20);
-		image.drawRect(Color.blue, x2, y2, 20, 20);
-		
-		
-		if(v1 == 0) x1 -= speed1;
-		else if(v1 == 1) x1 += speed1;
-		else if(v1 == 2) y1 -= speed1;
-		else if(v1 == 3) y1 += speed1;
-		
-		if(v2 == 0) x2 -= speed2;
-		else if(v2 == 1) x2 += speed2;
-		else if(v2 == 2) y2 -= speed2;
-		else if(v2 == 3) y2 += speed2;
+		image.drawRect(Color.red, x1, y1, size1, size1); 
+		image.drawRect(Color.blue, x2, y2, size2, size2);
+		image.drawRect(Color.white, 100, 100, 50, 50);
+		for(int i = 0; i < lineX1.size() - 1; i++) {
+
+				image.drawRect(Color.pink, lineX1.get(i), lineY1.get(i),
+										   lineX1.get(i+1) - lineX1.get(i), lineY1.get(i+1) - lineY1.get(i));
+			System.out.println("sdfsdf");
+		}
+		for(int i = 0; i < lineX2.size() - 1; i++) {
 			
+		}
+		
+		if(v1Last != v1) {
+			lineX1.add(x1);
+			lineY1.add(y1);System.out.println(x1 + " " + y1);
+		}
+			
+		if(v1 == 0) y1 -= speed1;
+		else if(v1 == 1) y1 += speed1;
+		else if(v1 == 2) x1 -= speed1;
+		else if(v1 == 3) x1 += speed1;
+		
+	
+		
+		if(v2 == 0) y2 -= speed2;
+		else if(v2 == 1) y2 += speed2;
+		else if(v2 == 2) x2 -= speed2;
+		else if(v2 == 3) x2 += speed2;
 		
 		
-		
+		v2Last = v2;
+		v1Last = v1;
 		
 		
 	}
 
-	
+	@Override
 	protected void processInput() {
 		while (handler.getKeys().size() > 0) {
 			int key = handler.getKeys().remove(0);
@@ -58,19 +88,15 @@ public class LightBike extends GameScreen {
 		
 	}
 
-
+	@Override
 	protected void checkEnd() {
 		// TODO Auto-generated method stub
 		
 	}
 
-
+	@Override
 	protected void resetGame() {
-		x1 = 400; y1 = 400;
-		x2 = 100; y2 = 100;
-		v1 = 0; v2 = 1;
-		speed1 = 10; speed2 = 10;
-		
+
 	}
 	
 	
