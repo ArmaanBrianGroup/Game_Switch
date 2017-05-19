117f44bc0f3c284c68b98b7da556fb5c21cef6c1
TheSpicyMango <pwnomancer@gmail.com>
2017-05-19 11:01:46 -0400

    Update LightBike.java

--- src/Games/LightBike.java
+++ src/Games/LightBike.java
@@ -10,64 +10,60 @@ import Main.Input_Handler;
 public class LightBike extends GameScreen {
 	private int x1, y1, x2, y2, speed1, speed2, size1, size2;
 	private int v1, v2, v1Last, v2Last; //0 up, 1 down, 2 left, 3 right
-	private ArrayList<Integer> lineX1, lineX2, lineY1, lineY2;
-	
+	private ArrayList<LazerPart> line1, line2;
+	private LazerPart lazer;
 	
 	public LightBike(Image image, Input_Handler handler) {
 		super(image, handler);
 
 		x1 = 400; y1 = 400;
-		x2 = 100; y2 = 100; System.out.println("CALLED");
+		x2 = 100; y2 = 100; 
 		v1 = 0; v2 = 1;
 		speed1 = 7; speed2 = 7;
 	
 		size1 = 20; size2 = 20;
-		
-		lineX1 = new ArrayList<Integer>();
-		lineY1 = new ArrayList<Integer>();
-		lineX2 = new ArrayList<Integer>();
-		lineX2 = new ArrayList<Integer>();
+		line1 = new ArrayList<LazerPart>();
+		line2 = new ArrayList<LazerPart>();
+		lazer = new LazerPart(0,0,size1);
 	}
 
 	protected void draw() {
 
 		image.drawRect(Color.red, x1, y1, size1, size1); 
 		image.drawRect(Color.blue, x2, y2, size2, size2);
-		image.drawRect(Color.white, 100, 100, 50, 50);
-		for(int i = 0; i < lineX1.size() - 1; i++) {
 
-				image.drawRect(Color.pink, lineX1.get(i), lineY1.get(i),
-										   lineX1.get(i+1) - lineX1.get(i), lineY1.get(i+1) - lineY1.get(i));
-		}
-		for(int i = 0; i < lineX2.size() - 1; i++) {
-			
-		}
-		
-		if(lineX1.size() > 1) {
-			image.drawRect(Color.pink, lineX1.get(0), lineY1.get(0),
-					   lineX1.get(0+1) - lineX1.get(0), lineY1.get(0+1) - lineY1.get(0));
-		}
+
 		if(v1Last != v1) {
-			lineX1.add(x1);
-			lineY1.add(y1);System.out.println(x1 + " " + y1);
+			LazerPart lazer = new LazerPart(x1, y1, size1);
+			line1.add(lazer);
+		
 		}
-			
-		if(v1 == 0) y1 -= speed1;
-		else if(v1 == 1) y1 += speed1;
-		else if(v1 == 2) x1 -= speed1;
-		else if(v1 == 3) x1 += speed1;
 		
-	
+		for(int i = 0; i < line1.size(); i++) {
+			line1.get(i).draw();
+			System.out.println("asdf");
+		}
 		
-		if(v2 == 0) y2 -= speed2;
-		else if(v2 == 1) y2 += speed2;
-		else if(v2 == 2) x2 -= speed2;
-		else if(v2 == 3) x2 += speed2;
+
+		if(v1 == 0) {
+			y1 -= speed1;
+			lazer.increaseY(-speed1);
+		}
+		else if(v1 == 1) {
+			y1 += speed1;
+			lazer.increaseY(speed1);
+		}
+		else if(v1 == 2) {
+			x1 -= speed1;
+			lazer.increaseX(-speed1);
+		}
+		else if(v1 == 3) {
+			x1 += speed1;
+			lazer.increaseX(speed1);
+		}
 		
 		
-		v2Last = v2;
 		v1Last = v1;
-		
 		
 	}
 
@@ -98,4 +94,34 @@ public class LightBike extends GameScreen {
 
 	}
 	
+	
+	public class LazerPart {
+		private int x0, y0, x, y;
+		private final int size;
+		
+		public LazerPart(int x, int y, int size) {
+			x0 = x;
+			y0 = y;
+			this.x = x;
+			this.y =y;
+			this.size = size;
+		}
+		
+		public void increaseX(int delta) {
+			x+=delta;
+		}
+		
+		public void increaseY(int delta) {
+			y+=delta;
+		}
+		
+		public void draw() {
+			if (x0 == x) {
+				image.drawRect(Color.WHITE, x0, y0, size, y0-y);
+			} else {
+				image.drawRect(Color.WHITE, x0, y0, x0-x, size);
+			}
+		}
+		
+	}
 }
